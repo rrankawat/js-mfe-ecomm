6b6d3fa6861466e2e68e6b1e51fdff2fda90a1d5
Rakesh Rankawat <rakesh.rankawat@outlook.com>
2023-01-22 22:12:48 +0530

    Async Script

--- products/src/index.js
+++ products/src/index.js
@@ -1,10 +1,1 @@
-import faker from 'faker'
-
-let products = ''
-
-for (let i = 0; i < 5; i++) {
-  const name = faker.commerce.productName()
-  products += `<li>${name}</li>`
-}
-
-document.querySelector('#dev-products').innerHTML = `<ul>${products}</ul>`
+import('./bootstrap')
